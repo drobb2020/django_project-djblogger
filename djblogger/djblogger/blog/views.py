90d66b4dc60d58fd29d7925cd9cbc993b8cc8d6f
David Robb <66834033+drobb2020@users.noreply.github.com>
2022-12-18 15:52:35 -0500

    Blog Listing build and added htmx

--- djblogger/djblogger/blog/views.py
+++ djblogger/djblogger/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class HomeView(ListView):
+    model = Post
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_template_names(self):
+        if self.request.htmx:
+            return "blog/components/post-list-elements.html"
+        return "blog/index.html"
--- djblogger/djblogger/urls.py
+++ djblogger/djblogger/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('__debug__/', include('debug_toolbar.urls')),
+    path('', include('djblogger.blog.urls')),
 ]
