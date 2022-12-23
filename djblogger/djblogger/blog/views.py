6a4feca3c49e45f1d9afbf7359829f2c11ada54c
David Robb <66834033+drobb2020@users.noreply.github.com>
2022-12-23 10:22:40 -0500

    Added get_queryset to filter out draft posts

--- djblogger/djblogger/blog/views.py
+++ djblogger/djblogger/blog/views.py
@@ -10,6 +10,10 @@ class HomeView(ListView):
     context_object_name = "posts"
     paginate_by = 10
 
+    def get_queryset(self):
+        x = Post.objects.filter(status='published')
+        return x
+
     def get_template_names(self):
         if self.request.htmx:
             return "blog/components/post-list-elements.html"
